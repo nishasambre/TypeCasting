c7868801293a7a0f1e64ce561a094df570dfc637
colleage <sambrenisha98@gmail.com>
2024-02-19 17:18:07 +0530

    commit here

---
+++ TypeCasting/src/com/typecasting/Explicit.java
@@ -0,0 +1,13 @@
+package com.typecasting;
+
+public class Explicit {
+
+	public static void main(String[] args) {
+		double a = 50.5;
+		int b = (int)a;
+		System.out.println(b);
+		
+
+	}
+
+}
---
+++ TypeCasting/src/com/typecasting/Implicit.java
@@ -0,0 +1,13 @@
+package com.typecasting;
+
+public class Implicit {
+
+	public static void main(String[] args) {
+		int a = 45;
+		double b = a;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
